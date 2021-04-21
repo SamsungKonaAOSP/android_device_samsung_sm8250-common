3d22ca42cdcfcedcbd50366aaec4269b3b799933
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-04-21 22:15:07 +0200

    sm8250-common: Use QTI USB init configs from USB HAL repo
    Change-Id: I966d251a2756c9d9e91f621cbc5872448d022de4

--- common.mk
+++ common.mk
@@ -417,8 +417,13 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
+
+PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
