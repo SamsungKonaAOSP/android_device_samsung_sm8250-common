5288a0c3c4f5b8b70060fa286bb52c6655bf7176
Rashed Abdel-Tawab <rashed@linux.com>
2022-06-15 14:45:20 +0200

    sm8250-common: Import audio@7.0 default impl
    Change-Id: I37dac8d63dff454a396e9c14a1062db7f4b2588d

--- common.mk
+++ common.mk
@@ -15,7 +15,7 @@ BOARD_SHIPPING_API_LEVEL := 30
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@6.0-impl \
+    android.hardware.audio@6.0-impl.samsung-sm8250 \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.r_submix.default \
