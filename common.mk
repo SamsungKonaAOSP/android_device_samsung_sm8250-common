243ea51a9851c1a4b45b291e78e450604d9551f4
Simon1511 <simon2002.schoenmackers@gmail.com>
2023-04-07 14:40:08 +0200

    sm8250-common: Import livedisplay service
    * from samsung sm7125
    
    Change-Id: I9e5db5b0cb155cfd86758eba995d92d15893da73

--- common.mk
+++ common.mk
@@ -166,6 +166,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.samsung
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service.samsung-qcom.sm8250
+
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
