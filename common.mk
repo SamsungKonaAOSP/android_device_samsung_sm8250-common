1dbd96de341acb430abf2674b63129ba88550a8d
Michael Bestas <mkbestas@lineageos.org>
2023-09-21 08:23:09 +0530

    sm8250-common: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- common.mk
+++ common.mk
@@ -105,7 +105,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1.vendor
+    android.hardware.health@2.1.vendor \
+    vendor.lineage.health-service.default
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
