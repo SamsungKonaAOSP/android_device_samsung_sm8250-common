263c3198ec941059ed46cbbf8be80f9bde1fc421
ata-kaner <ata_kaner@outlook.com>
2024-01-07 11:20:21 +0300

    sm8250-common: Use blobbed Gatekeeper HAL
    * Samsung added a function called isMdfppGatekeeper() for registering HAL as default or mdfpp. AOSP gatekeeper doesn't support that kind of approach and we need to register gatekeeper-qti as default. Use blobbed gatekeeper until a proper gatekeeper mdfpp bringup.
    
    * This affects all Samsung SM8250 platform.
    
    Change-Id: I3da59f9f2cfd024af1c3103cedcf1c21b7ac4c91
    Signed-off-by: ata-kaner <ata_kaner@outlook.com>

--- common.mk
+++ common.mk
@@ -152,8 +152,7 @@ PRODUCT_PACKAGES += \
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service
+    android.hardware.gatekeeper@1.0.vendor
 
 # GNSS
 PRODUCT_PACKAGES += \
