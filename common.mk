9c30e6bc57bf00664853268190b31272e9d6a21a
ata-kaner <ata.kaner03@gmail.com>
2024-05-28 18:52:02 +0300

    sm8250-common: Build SamsungDAP
    Change-Id: I1bf2a6fb88a629b548a3cb0eb9bdbebe6a8d58e9

--- common.mk
+++ common.mk
@@ -34,7 +34,10 @@ PRODUCT_PACKAGES += \
     libreverbwrapper \
     libtinycompress \
     libvisualizer \
-    libvolumelistener
+    libvolumelistener \
+    SamsungDAP
+
+TARGET_EXCLUDES_AUDIOFX := true
 
 # Audio configs
 PRODUCT_COPY_FILES += \
