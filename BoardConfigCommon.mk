eef388e13082a275fb1770f0f692f5357a62cead
ata-kaner <ata.kaner03@gmail.com>
2024-09-22 12:15:15 +0300

    sm8250-common: Setup proprietary files
    Change-Id: If0ee8b2bdc209dfd605eefd92e42a7cb45b4f174

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,6 +123,9 @@ TARGET_USERIMAGES_USE_F2FS   := true
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
+# Security patch
+VENDOR_SECURITY_PATCH := 2024-07-01
+
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
