9288c279a8511098ef3bd566e9e8eb3a0c38dc51
ata-kaner <ata.kaner03@gmail.com>
2024-10-05 13:02:10 +0300

    sm8250-common: Switch to QTI usb hal
    Change-Id: I574c182f3ef6dd95b3c91c813dacd227d23e71a0

--- common.mk
+++ common.mk
@@ -405,7 +405,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.samsung \
+    android.hardware.usb@1.3-service-qti
     init.qcom.usb.rc \
     init.qcom.usb.sh
 
